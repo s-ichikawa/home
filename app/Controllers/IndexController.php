cddea3a69372e74c1407897d196111aa5ffcbe80
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-03-05 22:50:42 +0900

    test

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,20 @@ class IndexController extends Controller
 
     public function get()
     {
-        return 'correct!!';
+        $this->limitIterator();
     }
 
+    private function limitIterator()
+    {
+        $array = new \ArrayIterator([
+            'a' => 'hoge',
+            0 => 'zero',
+            'b' => 'foo',
+            1 => 'bar',
+        ]);
+
+        foreach (new \LimitIterator($array, 1, 2) as $key => $val) {
+            var_dump($key . ':' . $val);
+        }
+    }
 }
