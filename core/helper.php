9af47e8c714768fedd310f13df3c82b31919aafa
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-03-23 14:59:41 +0900

    revise conflict

--- core/helper.php
+++ core/helper.php
@@ -32,8 +32,6 @@ if (!function_exists('resources_path')) {
 
 function route()
 {
-    static $root;
-    return $root ?? $root = new \Sichikawa\Home\Core\Route();
     static $route;
     return $route ?? $route = new \Sichikawa\Home\Core\Route();
 
