d76b67591168b4a7b9bd25f9ff39ce2f2a8873ec
Shingo Ichikawa <ichikawa.shingo.0829@gmail.com>
2017-04-28 17:42:57 +0900

    Update packagist_badges_stats.php

--- resources/php/packagist_badges_stats.php
+++ resources/php/packagist_badges_stats.php
@@ -1,20 +1,53 @@
 <?php
+use GuzzleHttp\Client;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+$cache_path = __DIR__ . '/list.json';
 
-$client = new \GuzzleHttp\Client();
+if (file_exists($cache_path) === false) {
+    touch($cache_path);
+}
 
-$response = $client->get('https://packagist.org/packages/list.json');
+$client = new Client();
+$json = file_get_contents($cache_path);
+if (empty($json)) {
+    $response = $client->get('https://packagist.org/packages/list.json');
 
-$json = json_decode($response->getBody()->getContents());
-foreach ($json->packageNames as $key => $value) {
+    if ($response) {
+        $json = $response->getBody()->getContents();
+        file_put_contents($cache_path, $json);
+    }
+    echo 'request succeeded.' . PHP_EOL;
+}
 
-    $source_url = sprintf('https://packagist.org/packages/%s.json', $value);
+$data = json_decode($json);
+$i = 0;
+foreach ($data->packageNames as $packageName) {
 
+    $source_url = sprintf('https://packagist.org/packages/%s.json', $packageName);
     $res = $client->get($source_url);
     $package = json_decode($res->getBody()->getContents())->package;
-
-    echo $package->repository;
-    break;
+    if (strpos($package->repository, 'github.com')) {
+        $contents_url = str_replace('github.com/', 'api.github.com/repos/', $package->repository) . '/contents';
+        $res = $client
+            ->get($contents_url)
+            ->getBody();
+        $contents = json_decode($res);
+        foreach($contents as $content) {
+            if (preg_match('/readme/i', $content->name)) {
+                $readme = $client->get($content->download_url)->getBody()->getContents();
+                preg_match_all('/\[\!\[(.*)\]\((.*)\)\]\((.*)\)/', $readme, $matches);
+                var_dump($matches);
+                $i++;
+                break;
+            }
+        }
+    } else {
+        echo 'unknown repository. => ' . $package->repository;
+    }
+    if ($i > 20) {
+        break;
+    }
 }
 
