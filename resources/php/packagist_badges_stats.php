6d614ccf33f3d3348baf84170e8f4990103b7a65
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-05-03 01:06:18 +0900

    use redis

--- resources/php/packagist_badges_stats.php
+++ resources/php/packagist_badges_stats.php
@@ -1,74 +1,187 @@
 <?php
+
+namespace Packagist\Stats;
+
+use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use Redis;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$env = require __DIR__ . '/../../.env.php';
 
-$id = $env['GITHUB_ID'];
-$secret = $env['GITHUB_PASSWD'];
-
-$redis = new Redis();
-$redis->connect('127.0.0.1');
-if (!$redis->ping()) {
-    throw new Exception('Redis cant connect.');
-}
-
-
-$client = new Client();
-if (!$json = $redis->get('list_json')) {
-    $response = $client->get('https://packagist.org/packages/list.json');
-
-    if ($response) {
-        $json = $response->getBody()->getContents();
-        $redis->set('list_json', $json);
+class Cache
+{
+    /**
+     * Cache constructor.
+     */
+    private function __construct()
+    {
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1');
+        if (!$this->redis->ping()) {
+            throw new Exception('Redis cant connect.');
+        }
     }
-    echo 'request succeeded.' . PHP_EOL;
+
+    public static function getInstance()
+    {
+        static $instance;
+        if ($instance) {
+            return $instance;
+        }
+        return $instance = new self();
+    }
+
+    public function get($key)
+    {
+        return $this->redis->get($key);
+    }
+
+    public function set($key, $val)
+    {
+        return $this->redis->set($key, $val);
+    }
 }
 
-$data = json_decode($json);
-$i = 0;
-foreach ($data->packageNames as $packageName) {
+class Packagist
+{
 
-    // test
-    $packageName = '0100dev/cakephp-rabbitmq';
+    /**
+     * Packagist constructor.
+     */
+    public function __construct()
+    {
+        $this->cache = Cache::getInstance();
+        $this->client = new Client();
+    }
 
-    $source_url = sprintf('https://packagist.org/packages/%s.json', $packageName);
-    echo $i . ': ' . $source_url . PHP_EOL;
-    $res = $client->get($source_url);
-    $package = json_decode($res->getBody()->getContents())->package;
-    if (strpos($package->repository, 'github.com')) {
+    public function getPackageNames()
+    {
+        if (!$json = $this->cache->get('list_json')) {
+            $response = $this->client->get('https://packagist.org/packages/list.json');
+
+            if ($response) {
+                $json = $response->getBody()->getContents();
+                $this->cache->set('list_json', $json);
+            }
+            echo 'request succeeded.' . PHP_EOL;
+        }
+
+        return json_decode($json)->packageNames;
+    }
+
+    public function getPackage($name)
+    {
+        $cache_key = 'packagist::package::' . $name;
+        if (!$json = $this->cache->get($cache_key)) {
+            $source_url = sprintf('https://packagist.org/packages/%s.json', $name);
+            $json = $this->client->get($source_url)->getBody()->getContents();
+            $this->cache->set($cache_key, $json);
+        }
+        return json_decode($json)->package ?? null;
+    }
+}
+
+class Github
+{
+
+    /**
+     * Packagist constructor.
+     */
+    public function __construct()
+    {
+        $this->cache = Cache::getInstance();
+        $this->client = new Client();
+
+
+        $env = require __DIR__ . '/../../.env.php';
+        $this->id = $env['GITHUB_ID'];
+        $this->secret = $env['GITHUB_PASSWD'];
+    }
+
+    public function getContents($package)
+    {
         try {
-            $contents_url = str_replace('github.com/', 'api.github.com/repos/', $package->repository) . '/contents?client_id=' . $id . '&client_secret=' . $secret;
-            $res = $client
-                ->get($contents_url)
-                ->getBody();
-            $contents = json_decode($res);
-        } catch (\GuzzleHttp\Exception\ClientException $exception) {
+            $cache_key = 'github::contents::' . $package->name;
+            if (!$json = $this->cache->get($cache_key)) {
+                $url = str_replace('github.com/', 'api.github.com/repos/', $package->repository)
+                    . '/contents?client_id=' . $this->id . '&client_secret=' . $this->secret;
+
+                $json = $this->client
+                    ->get($url)
+                    ->getBody()->getContents();
+
+                $this->cache->set($cache_key, $json);
+            }
+
+            return json_decode($json);
+        } catch (ClientException $exception) {
             echo $exception . PHP_EOL;
+        }
+        return null;
+    }
+
+    public function download($url)
+    {
+        try {
+            return $this->client->get($url)->getBody()->getContents();
+        } catch (ConnectException $exception) {
+            echo $exception . PHP_EOL;
+        }
+        return null;
+    }
+}
+
+function getBadges($repository, $readme)
+{
+    preg_match_all('/\[\!\[(.*?)\]\((.*?)\)\]\((.*?)\)/', $readme, $matches);
+
+    $badges = $matches[2];
+    $services = $matches[3];
+    $count = count($badges);
+    for ($j = 0; $j < $count; $j++) {
+        $badge = str_replace($repository, 'user/repo', $badges[$j]);
+        $service = str_replace($repository, 'user/repo', $services[$j]);
+
+        yield [
+            'badge'   => $badge,
+            'service' => $service,
+        ];
+    }
+}
+
+$packagist = new Packagist();
+$github = new Github();
+$i = 0;
+foreach ($packagist->getPackageNames() as $packageName) {
+    echo ++$i . ':' . $packageName . PHP_EOL;
+    // test
+//    $packageName = '0100dev/cakephp-rabbitmq';
+
+    if (!$package = $packagist->getPackage($packageName)) {
+        echo 'no package.';
+        continue;
+    }
+    if (strpos($package->repository, 'github.com')) {
+
+        if (!$contents = $github->getContents($package)) {
+            echo 'no contents.' . PHP_EOL;
             continue;
         }
 
         foreach ($contents as $content) {
             if (preg_match('/readme/i', $content->name)) {
-                $readme = $client->get($content->download_url)->getBody()->getContents();
-                preg_match_all('/\[\!\[(.*?)\]\((.*?)\)\]\((.*?)\)/', $readme, $matches);
-
-                $badges = $matches[2];
-                $services = $matches[3];
-                $count = count($badges);
-                for ($j = 0; $j < $count; $j++) {
-                    $badge = str_replace($package->repository, 'user/repo', $badges[$j]);
-                    $service = str_replace($package->repository, 'user/repo', $services[$j]);
-
-                    var_dump($badge, $service);
+                if (!$readme = $github->download($content->download_url)) {
+                    break;
                 }
-
-                $i++;
+                foreach (getBadges($package->repository, $readme) as $badge) {
+                    var_dump($badge);
+                }
                 break;
             }
         }
-        break;
     } else {
         echo 'unknown repository. => ' . $package->repository;
     }
