1aa70ae630efb101c72d2ac39f2ef293528c701b
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-05-26 20:00:24 +0900

    redis controller

--- app/Controllers/RedisController.php
+++ app/Controllers/RedisController.php
@@ -9,24 +9,8 @@ class RedisController extends Controller
 
     public function get()
     {
-        var_dump($this->getRequest()->get('test'));
+        $redis = new \Redis();
+
     }
 
-    public function post()
-    {
-        echo 'Yes POST';
-    }
-
-    public function delete()
-    {
-        echo __FUNCTION__;
-    }
-
-    public function draw_search()
-    {
-        $config = require(base_path('.env.php'));
-        (new View())->render('draw_search', [
-            'key' => $config['GOOGLE_MAP_KEY']
-        ]);
-    }
 }
